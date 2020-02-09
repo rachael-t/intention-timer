23075cc922d08ae661e84fc60f796c26d89a1efe
carflor <cfdeclet@gmail.com>
2020-02-09 12:40:33 -0700

    Added function for category error message

--- main.js
+++ main.js
@@ -1,213 +1,134 @@
-// IDEAL FN for BUTTONS RESPONSE
-// Sets EventListener delegated to the node above buttons. 
-// <section> CATEGORY BUTTONS </section> needs to respond
-// as an Event Listener for any clicking within the buttons.
-// Buttons probably belong to an array of responsive event 
-// that probably contain an array of affected variables 
-// being the border, the img and the text of the button. 
-// These three variables will change in color corresponding 
-// to their button type. Study is green, meditate is purple, 
-// exercise is red. 
-// Study Event Delegation and Bubbling. Event Object behaviour.
-
-
-
-// BUTTON RESPONSE SKETCH:
-// VARIABLES 
+// BUTTON RESPONSE:
 var clickStudy = document.querySelector('.study-button');
 var clickMeditate = document.querySelector('.meditate-button');
 var clickExercise = document.querySelector('.exercise-button');
+var buttonSelected = "";
 
-// event listeners
 clickStudy.addEventListener('click', displayColorStudy)
 clickMeditate.addEventListener('click', displayColorMeditate)
 clickExercise.addEventListener('click', displayColorExercise)
 
-// fn
 function displayColorStudy() {
     clickStudy.innerHTML = `<button class="activity-button study-button study-active">
-          <img src="./assets/study-active.svg" 
-          alt="study activate logo">Study</button>`
+          <img src="./assets/study-active.svg"
+          alt="study activate logo">Study</button>`;
+    clickMeditate.innerHTML =`
+    <button class="activity-button meditate-button">
+          <img src="./assets/meditate.svg"
+          alt="meditate logo">Meditate</button>`;
+    clickExercise.innerHTML =`
+    <button class="activity-button exercise-button">
+          <img src="./assets/exercise.svg"
+          alt="exercise logo">Exercise</button>`;
+    buttonSelected = "study";
 };
 
 function displayColorMeditate() {
     clickMeditate.innerHTML =`
     <button class="activity-button meditate-button meditate-active">
-          <img src="./assets/meditate-active.svg" 
-          alt="meditate activate logo">Meditate</button>`
+          <img src="./assets/meditate-active.svg"
+          alt="meditate activate logo">Meditate</button>`;
+    clickStudy.innerHTML = `<button class="activity-button study-button">
+          <img src="./assets/study.svg"
+          alt="study logo">Study</button>`;
+    clickExercise.innerHTML =`
+    <button class="activity-button exercise-button">
+          <img src="./assets/exercise.svg"
+          alt="exercise logo">Exercise</button>`;
+    buttonSelected = "meditate";
 };
 
 function displayColorExercise() {
     clickExercise.innerHTML =`
     <button class="activity-button exercise-button exercise-active">
-          <img src="./assets/exercise-active.svg" 
-          alt="exercise activate logo">Exercise</button>`
+          <img src="./assets/exercise-active.svg"
+          alt="exercise activate logo">Exercise</button>`;
+    clickStudy.innerHTML = `<button class="activity-button study-button">
+          <img src="./assets/study.svg"
+          alt="study logo">Study</button>`;
+    clickMeditate.innerHTML =`
+    <button class="activity-button meditate-button">
+          <img src="./assets/meditate.svg"
+          alt="meditate logo">Meditate</button>`;
+    buttonSelected = "exercise";
 };
 
+// Page load to hide category warning
+var categoryWarning = document.querySelector('#category-warning');
 
-// get var for start-button class
-// get var for warning-message class
-// get var for no-warning class
+categoryWarning.addEventListener('load', hideCategoryWarning)
 
+function hideCategoryWarning() {
+      document.getElementById('category-warning').style.visibility = "hidden";
+      console.log('cat is hidden');
+};
 
-var accomplishInput = document.getElementById('.accomplish');
+// Warning message below category/input fields
+var accomplishInput = document.getElementById('accomplish');
 var clickStart = document.querySelector('.start-button');
+
+clickStart.addEventListener('click', showCategoryWarning)
+
+function showCategoryWarning() {
+
+  if(buttonSelected === "") {
+    document.getElementById('category-warning').style.visibility = "visible";
+    console.log('cat should be visible');
+  }
+};
+
 
 clickStart.addEventListener('click', showWarning)
 
-
 function showWarning() {
-    if(accomplishInput.value === 0) {
-        handleNoWarning.display.style = "block";
+    if(accomplishInput.value === "") {
+    var handleNoWarning = document.querySelector('.no-warning')
+    handleNoWarning.style.display = "block";
     }
 };
 
-function checkNum(seconds) {
-    var minInput = getElementById('minutes');
-    
-    if(isNan(minInput.value)) {
-        alert('not a number');
+clickStart.addEventListener('click', showMinWarning)
+
+function showMinWarning() {
+    var minInput = document.forms["intention-form"]["minutes"].value;
+    if(isNaN(minInput)) {
+    var handleNoWarningMinutes = document.querySelector('#no-warning-minutes')
+    handleNoWarningMinutes.style.display = "block";
     }
-}
+};
 
+clickStart.addEventListener('click', showSecWarning)
 
-function check_field(id) {
-    var field = document.getElementById(id);
-
-    if (isNaN(field.value)) {
-        alert('not a number');
+function showSecWarning() {
+    var secInput = document.forms["intention-form"]["seconds"].value;
+    if(isNaN(secInput)) {
+    var handleNoWarningSeconds = document.querySelector('#no-warning-seconds')
+    handleNoWarningSeconds.style.display = "block";
     }
-}
+};
 
+//Function for switching pages when start button is click
+// var timerPage = document.querySelector('.timer-page');
+// var activityPage = document.querySelector('.activity-page');
+// var startButton = document.querySelector('.start-button');
+// var newActivityHeader = document.querySelector('.new-activity-header');
+// var currentActivityHeader = document.querySelector('.current-activity-header');
 
+// startButton.addEventListener('click', handlePageSwitch)
 
+// function handlePageSwitch() {
+//   activityPage.classList.add('hidden');
+//   timerPage.classList.remove('hidden');
+//   newActivityHeader.classList.add('hidden');
+//   currentActivityHeader.classList.remove('hidden');
+// };
 
+// Sketch for Input Transfer
+// var accomplishInput = document.querySelector('#accomplish');
+// var accDisplay = document.querySelector('#accomplish-display');
 
+// startButton.addEventListener('click', getInput)
 
-// var handleWarningMessage = document.getElementsByClassName('.warning-message');
-// var handleNoWarning = document.getElementsByClassName('.no-warning');
-
-
-
-
-
-
-// function showWarning() {
-//    if(accomplishInput.value.length === 0) {
-//        handleWarningMessage.disabled = true;
-//        console.log('nothing is there');
-//    }
+// function getInput() {
+//     accomplishInput.innerText = accDisplay.value;
 // }
-
-// if(accomplishInput.value.length === "") {
-//     handleNoWarning.display.style = "block";
-// }
-
-// get start button
-// var minutesInput = document.querySelector('.second-input');
-
-// var minutesObject {
-//     input: 1...60;
-
-// }
-
-// add function to only accept integers without e
-
-
-// var
-
-// funtion numVerify() {
-//     if (minutesInput) 
-// }
-
-
-
-
-// NEW STUFF ATTEMPT:
-
-// var timerPage = document.getElementsByClassName('timer-cat-shell');
-// var mainPage = document.getElementsByClassName('cat-shell');
-// var mainButton = document.getElementsByClassName('start-button');
-
-// startButton.addEventListener('click', handleStartButton)
-
-// function handleStartButton(e) {
-//   clickStartButton.classList.hide('.cat-shell');
-
-//   if(e.target.matches('mainButton')) {
-//       e.target.classList.style.display.block('timer-cat-shell');
-//       e.target.classList.style.display.none('cat-shell');
-//   }
-// } 
-// double check
-//document.getElementByClassName("warning-message").style.display = "none";
-
-
-
-
-
-// if(e.target.matches('ul')) {
-    //     e.target.classList.add("blue-border");
-    //   }
-
-
-
-
-
-
-
-
-// SPECS:
-// Study: #B3FD78
-// Meditate: #C278FD
-// Exercise: #FD8078
-// Error: #EFB7EC
-
-// OTHER IDEAS:
-// idea for h1 to be a home button
-// Past Activities default message can be 
-// a if/else statement
-
-// User Story:
-// - User selects category
-// - User types information in accomplish-input field
-// - User types amount of minutes into input field. Data is verified by validating function.
-// - User types amount of seconds into input field. Data is verified by validating function. 
-// Variables:
-// accomplish-input
-// minutes-input
-// seconds-input
-// example: var seconds = document.querySelector('.seconds-input input')
-// querySelector 
-// getElementsByClassName -- then our input fields could all have the same class name and we take/verify that info at once
-// querySelectorAll (pg 193)
-// need to add in classes for the actual input element for the two variables being targeted.
-// class = "minutes"
-// class = "seconds"
-// FUNCTION:
-// (study method that filters function?)
-// Idea 1:
-// if input.value > 0 || input.value < 59
-// return
-// else return alert "need to be number"
-// (issues with this idea - too many assumptions - not specific enough)
-// Idea 2: 
-// using pages 607 - 610 in JS book
-// HTML5 element required = true/false
-// Where testing true/false against number value
-// function validateRequired () {
-//   isEmpty ()
-// }
-// in the <input> tag for HTML add in required 
-// object to validate data type
-// look into .filter() 
-// pg 139
-// 132 - method: isNan() checks if the value is not a number
-
-
-
-// p.124 - could use window.alerg() method to create a dialog box with 
-// error message that has to be click before continuing with submit
-
-// p.126 could use a document.createElement() method to create a text in red that alerts
-// the user that the input is invalid.
