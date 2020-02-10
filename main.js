9aa16fda4f8d09e018c1c3a13449f9112dd8f51d
Rachael Thomas <rnthomas001@gmail.com>
2020-02-10 16:53:34 -0700

    Fixed indents in JS for readability

--- main.js
+++ main.js
@@ -29,87 +29,87 @@ clickStart.addEventListener('click', getInput)
 
 // Functions:
 function displayColorStudy() {
-    clickStudy.innerHTML = `<button class="activity-button study-button study-active">
-          <img src="./assets/study-active.svg"
-          alt="study activate logo">Study</button>`;
-    clickMeditate.innerHTML =`
-    <button class="activity-button meditate-button">
-          <img src="./assets/meditate.svg"
-          alt="meditate logo">Meditate</button>`;
-    clickExercise.innerHTML =`
-    <button class="activity-button exercise-button">
-          <img src="./assets/exercise.svg"
-          alt="exercise logo">Exercise</button>`;
-    buttonSelected = "study";
+  clickStudy.innerHTML = `<button class="activity-button study-button study-active">
+    <img src="./assets/study-active.svg"
+    alt="study activate logo">Study</button>`;
+  clickMeditate.innerHTML =`
+  <button class="activity-button meditate-button">
+    <img src="./assets/meditate.svg"
+    alt="meditate logo">Meditate</button>`;
+  clickExercise.innerHTML =`
+  <button class="activity-button exercise-button">
+    <img src="./assets/exercise.svg"
+    alt="exercise logo">Exercise</button>`;
+  buttonSelected = "study";
 };
 
 function displayColorMeditate() {
-    clickMeditate.innerHTML =`
-    <button class="activity-button meditate-button meditate-active">
-          <img src="./assets/meditate-active.svg"
-          alt="meditate activate logo">Meditate</button>`;
-    clickStudy.innerHTML = `<button class="activity-button study-button">
-          <img src="./assets/study.svg"
-          alt="study logo">Study</button>`;
-    clickExercise.innerHTML =`
-    <button class="activity-button exercise-button">
-          <img src="./assets/exercise.svg"
-          alt="exercise logo">Exercise</button>`;
-    buttonSelected = "meditate";
+  clickMeditate.innerHTML =`
+  <button class="activity-button meditate-button meditate-active">
+    <img src="./assets/meditate-active.svg"
+    alt="meditate activate logo">Meditate</button>`;
+  clickStudy.innerHTML = `<button class="activity-button study-button">
+    <img src="./assets/study.svg"
+    alt="study logo">Study</button>`;
+  clickExercise.innerHTML =`
+  <button class="activity-button exercise-button">
+    <img src="./assets/exercise.svg"
+    alt="exercise logo">Exercise</button>`;
+  buttonSelected = "meditate";
 };
 
 function displayColorExercise() {
-    clickExercise.innerHTML =`
-    <button class="activity-button exercise-button exercise-active">
-          <img src="./assets/exercise-active.svg"
-          alt="exercise activate logo">Exercise</button>`;
-    clickStudy.innerHTML = `<button class="activity-button study-button">
-          <img src="./assets/study.svg"
-          alt="study logo">Study</button>`;
-    clickMeditate.innerHTML =`
-    <button class="activity-button meditate-button">
-          <img src="./assets/meditate.svg"
-          alt="meditate logo">Meditate</button>`;
-    buttonSelected = "exercise";
+  clickExercise.innerHTML =`
+  <button class="activity-button exercise-button exercise-active">
+    <img src="./assets/exercise-active.svg"
+    alt="exercise activate logo">Exercise</button>`;
+  clickStudy.innerHTML = `<button class="activity-button study-button">
+    <img src="./assets/study.svg"
+    alt="study logo">Study</button>`;
+  clickMeditate.innerHTML =`
+  <button class="activity-button meditate-button">
+    <img src="./assets/meditate.svg"
+    alt="meditate logo">Meditate</button>`;
+  buttonSelected = "exercise";
 };
 
 function hideCategoryWarning() {
-      document.getElementById('category-warning').style.visibility = "hidden";
-      console.log('cat is hidden');
+  document.getElementById('category-warning').style.visibility = "hidden";
+  console.log('cat is hidden');
 };
 
 function handlePageSwitch() {
-      var isSelect = true;
-      var isFilled = true;
-      var hasMin = true;
-      var hasSec = true;
-      if(buttonSelected === "") {
-      document.getElementById('category-warning').style.visibility = "visible";
-      isSelect = false;
-      }
-      if(accomplishInput.value === "") {
-      var handleNoWarning = document.querySelector('.no-warning')
-      handleNoWarning.style.display = "block";
-            isFilled = false;
-      }
-      var minInput = document.forms["intention-form"]["minutes"].value;
-      if(isNaN(minInput)) {
-       var handleNoWarningMinutes = document.querySelector('#no-warning-minutes')
-      handleNoWarningMinutes.style.display = "block";
-      hasMin = false;
-      }
-      var secInput = document.forms["intention-form"]["seconds"].value;
-      if(isNaN(secInput)) {
-      var handleNoWarningSeconds = document.querySelector('#no-warning-seconds')
-      handleNoWarningSeconds.style.display = "block";
-      hasSec = false;
-      }
-      else if(isSelect && isFilled && hasMin && hasSec) {
-      activityPage.classList.add('hidden');
-      timerPage.classList.remove('hidden');
-      newActivityHeader.classList.add('hidden');
-      currentActivityHeader.classList.remove('hidden');
-      };
+  var isSelect = true;
+  var isFilled = true;
+  var hasMin = true;
+  var hasSec = true;
+  if(buttonSelected === "") {
+    document.getElementById('category-warning').style.visibility = "visible";
+    isSelect = false;
+  }
+  if(accomplishInput.value === "") {
+    var handleNoWarning = document.querySelector('.no-warning')
+    handleNoWarning.style.display = "block";
+    isFilled = false;
+  }
+  var minInput = document.forms["intention-form"]["minutes"].value;
+  if(isNaN(minInput)) {
+   var handleNoWarningMinutes = document.querySelector('#no-warning-minutes')
+   handleNoWarningMinutes.style.display = "block";
+   hasMin = false;
+  }
+  var secInput = document.forms["intention-form"]["seconds"].value;
+  if(isNaN(secInput)) {
+    var handleNoWarningSeconds = document.querySelector('#no-warning-seconds')
+    handleNoWarningSeconds.style.display = "block";
+    hasSec = false;
+  }
+  else if(isSelect && isFilled && hasMin && hasSec) {
+    activityPage.classList.add('hidden');
+    timerPage.classList.remove('hidden');
+    newActivityHeader.classList.add('hidden');
+    currentActivityHeader.classList.remove('hidden');
+  };
 };
 
 function timerColor() {
